61023614aa7d32467f10b2cf6a26d212aa988222
EnricoBrandinelli <enrico.brandinelli.stud@ispascalcomandini.it>
2025-10-23 11:15:45 +0200

    Aggiungere i file di progetto.

---
+++ src/BlaisePascal.Grades.Domain/Grade.cs
@@ -0,0 +1,8 @@
+﻿namespace BlaisePascal.Grades.Domain
+{
+    public class Grade
+    {
+        float Value { get; private set; }
+        string Subject { get; private set; }
+    }
+}
---
+++ src/BlaisePascal.Grades.Domain/Student.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlaisePascal.Grades.Domain
+{
+    public class Student
+    {
+    }
+}
